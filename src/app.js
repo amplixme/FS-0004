6e0d50a803eb541c2b9975defa40158a98d701a6
thiago <thiagogalleguillo9@gmail.com>
2026-06-16 17:29:45 -0300

    feature: card-01

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+require ('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+//Prueba
+app.get('/', (req, res) => {
+    res.json({
+        status: 'Ok',
+        message: 'Servidor express activo'
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor Express corriendo en http://localhost:${PORT}`);
+});
