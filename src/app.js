63e8a4694b30e8f406af97abe225fdac0010f2ea
thiago <thiagogalleguillo9@gmail.com>
2026-06-17 20:28:56 -0300

    feat: errors configs middleware and prisma errors

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { errorHandler } from './middlewares/error.middleware.js';
+
 dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-//middlewares
+//Middlewares
 app.use(cors());
 app.use(express.json());
 
-//Prueba
+//Prueba inicial
 app.get('/', (req, res) => {
     res.json({
         status: 'Ok',
@@ -18,6 +20,10 @@ app.get('/', (req, res) => {
     })
 })
 
+//Middleware de manejo de errores
+app.use(errorHandler);
+
+//Server escuchando
 app.listen(PORT, () => {
     console.log(`Servidor Express corriendo en http://localhost:${PORT}`);
 });
---
+++ src/middlewares/error.middleware.js
@@ -0,0 +1,16 @@
+import { error } from '../utils/response.js';
+
+export function errorHandler(err, req, res, next){
+    console.error(err);
+    
+    //Manejo de errores de prisma
+    if(err.code === 'P2002'){
+        return error(res, 'Ya existe un registro con esos datos',409);
+    }
+
+    if(err.code === 'P2025'){
+        return error(res, 'Registro no encontrado',404);
+    }
+
+    return error(res, err.message || 'Error interno del servidor', 500);
+}
