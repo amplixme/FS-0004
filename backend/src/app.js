658e6e5290ac8e976b54f4cf1c1e205deb46a119
thiago <thiagogalleguillo9@gmail.com>
2026-06-22 23:13:34 -0300

    chore: move backend files into backend/ folder

---
+++ backend/src/app.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { errorHandler } from './middlewares/error.middleware.js';
+import routes from './routes/index.js';
+
+dotenv.config()
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//Middlewares
+app.use(cors());
+app.use(express.json());
+app.use('/api', routes);
+
+//Ruta inicial
+app.get('/', (req, res) => {
+    res.json({
+        status: 'Ok',
+        message: 'Servidor express activo'
+    })
+})
+
+//Middleware de manejo de errores
+app.use(errorHandler);
+
+//Server escuchando
+app.listen(PORT, () => {
+    console.log(`Servidor Express corriendo en http://localhost:${PORT}`);
+});
