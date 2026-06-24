47ed81ee7e3014ada5eddb114bda331ae70e410f
thiago <thiagogalleguillo9@gmail.com>
2026-06-24 13:13:04 -0300

    "fix: imports in routes"

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -2,10 +2,13 @@ import { Router } from 'express';
 import { validate } from '../middlewares/validate.middleware.js';
 import { registerSchema } from '../schemas/auth.schema.js';
 import { register, login } from '../controllers/auth.controller.js';
+import { loginSchema } from '../schemas/auth.schema.js';
+import { login } from '../controllers/auth.controller.js';
+
 
 const router = Router();
 
 router.post('/register', validate(registerSchema), register);
-router.post('/login', login);
+router.post('/login', validate(loginSchema), login);
 
 export default router;
