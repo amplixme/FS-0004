2d6fe587afd89e767e480833b0d53a01e8747610
thiago <thiagogalleguillo9@gmail.com>
2026-06-24 13:14:11 -0300

    "fix: add loginSchema"

--- backend/src/schemas/auth.schema.js
+++ backend/src/schemas/auth.schema.js
@@ -8,3 +8,10 @@ export const registerSchema = z.object({
 	password: z.string({ required_error: 'La contraseña es obligatoria' })
 	    .min(6, 'La contraseña debe tener al menos 6 caracteres')
 });
+
+export const loginSchema = z.object({
+	email: z.string({ required_error: 'El email es obligatorio' })
+        .email('El email no es válido'),
+    password: z.string({ required_error: 'La contraseña es obligatoria' })
+        .min(6, 'La contraseña debe tener al menos 6 caracteres')
+})
