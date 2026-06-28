e6ea682065487b790e59a7201ad9fd1e9b36524a
dianaguerrero <dguerreropacheco@gmail.com>
2026-06-28 17:45:07 -0500

    feature:  agregar middleware para required role/card-10

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,12 +1,23 @@
-import { Router } from 'express';
-import { validate } from '../middlewares/validate.middleware.js';
-import { registerSchema } from '../schemas/auth.schema.js';
-import { register, login } from '../controllers/auth.controller.js';
-import { loginSchema } from '../schemas/auth.schema.js';
+import { Router } from "express";
+import { validate } from "../middlewares/validate.middleware.js";
+import { registerSchema } from "../schemas/auth.schema.js";
+import { register, login } from "../controllers/auth.controller.js";
+import { loginSchema } from "../schemas/auth.schema.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { requireRole } from "../middlewares/role.middleware.js";
 
 const router = Router();
 
-router.post('/register', validate(registerSchema), register);
-router.post('/login', validate(loginSchema), login);
+router.post("/register", validate(registerSchema), register);
+router.post("/login", validate(loginSchema), login);
+
+
+//Futura ruta//
+router.get("/admin-test", authMiddleware, requireRole("ADMIN"), (req, res) => {
+  res.json({
+    message: "Acceso autorizado",
+    user: req.user,
+  });
+});
 
 export default router;
