6d15b7fb8504d3b331d81f4d901cd4071a240aae
thiago <thiagogalleguillo9@gmail.com>
2026-06-30 13:12:53 -0300

    "fix:/api/auth/login"

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -28,7 +28,7 @@ export default function Login() {
     setLoading(true);
 
     try {
-      const { data } = await api.post("api/auth/login", form);
+      const { data } = await api.post("/api/auth/login", form);
 
       login(data.token, data.user);
 
